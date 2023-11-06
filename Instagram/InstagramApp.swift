b171e532adc0b0e970d3a7710f1fe2259142bf43
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-11-06 14:05:34 +0300

    Create repository

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Kate Kashko on 6.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
