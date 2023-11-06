0a6254b2f9c996e3718ff53687cf523992f2a0d4
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-11-06 16:23:25 +0300

    Complete ProfileView layout

--- Instagram/Core/Profile/ProfileView.swift
+++ Instagram/Core/Profile/ProfileView.swift
@@ -8,88 +8,91 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    
     var body: some View {
-        VStack{
-            //header
-            
-            VStack(spacing: 10){
-                // pic and stars
+        NavigationView {
+            ScrollView{
+                //header
                 
-                HStack{
-                    Image("avatar")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 80, height: 80)
-                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    Spacer()
-                    HStack(spacing: 8) {
-                        VStack{
-                            Text("3")
-                                .font(.subheadline)
-                                .fontWeight(.semibold)
-
-                            Text("Post")
-                                .font(.footnote)
-                        }
-                        .frame(width: 76)
-                        
-                        VStack{
-                            Text("3")
-                                .font(.subheadline)
-                                .fontWeight(.semibold)
-
-                            Text("Followers")
-                                .font(.footnote)
-                        }
-                        .frame(width: 76)
-
-                        
-                        VStack{
-                            Text("3")
-                                .font(.subheadline)
-                                .fontWeight(.semibold)
-
-                            Text("Following")
-                                .font(.footnote)
-                        }
-                        .frame(width: 76)
-                    }
-
-                }
-                .padding(.horizontal)
-             
-                // name and bio
-                
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Name Surname")
-                        .font(.footnote)
-                        .fontWeight(.semibold)
-                    Text("Lorem lorem")
-                        .font(.footnote)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-                
-                // action button
-                
-                Button{
+                VStack(spacing: 10){
+                    // pic and stars
                     
-                } label: {
-                    Text("Edit profile")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .frame(width: 360, height: 32)
-                        .foregroundColor(.black)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 6)
-                                .stroke(Color.gray, lineWidth: 1)
-                        )
+                    HStack{
+                        Image("1")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        Spacer()
+                        
+                        HStack(spacing: 8) {
+                            UserStatView(value: 3, title: "Posts")
+                            UserStatView(value: 3, title: "Followers")
+                            UserStatView(value: 3, title: "Following")
+                        }
+
+                    }
+                    .padding(.horizontal)
+                 
+                    // name and bio
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Name Surname")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        Text("Lorem lorem")
+                            .font(.footnote)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                    // action button
+                    
+                    Button{
+                        
+                    } label: {
+                        Text("Edit profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundColor(.black)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+                    }
+                    
+                    Divider()
                 }
                 
-                Divider()
+                //post grid view
+                LazyVGrid(columns: gridItems, spacing: 1) {
+                    ForEach(0...15, id: \.self) { index in
+                        Image("2")
+                            .resizable()
+                            .scaledToFill()
+                        
+                    }
+                    
+                }
             }
-            
-            //post grid view
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundColor(.black)
+                    }
+                }
+            }
         }
     }
 }
