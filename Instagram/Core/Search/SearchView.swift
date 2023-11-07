a2119b0a09f0281bbe5ece606580287e0a332ad7
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-11-07 10:43:50 +0300

    Add SearchView

--- Instagram/Core/Search/SearchView.swift
+++ Instagram/Core/Search/SearchView.swift
@@ -7,9 +7,41 @@
 
 import SwiftUI
 
+
 struct SearchView: View {
+    
+    @State private var searchText = ""
+    
+   
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(0...15, id:\.self) { user in
+                        HStack {
+                            Image("3")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                            VStack(alignment: .leading) {
+                                Text("Lorem")
+                                    .fontWeight(.semibold)
+                                Text("Lorem Lorem")
+                            }
+                            .font(.footnote)
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                        
+                    }
+                }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search...")
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
